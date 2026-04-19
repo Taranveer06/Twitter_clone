8174fb4a20972868681525e8c534f72fb86bb97e
Shekhar <shekharjadhav9766@gmail.com>
2026-04-19 23:57:22 +0530

    Making of Post section

---
+++ src/Tweet.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Tweet.css"
+function Tweet(){
+    return (
+        <div className="Tweet">
+            <form>
+                <div className="tweet_input">
+
+                </div>
+                <button>Tweet</button>
+            </form>
+        </div>
+    )
+}
+
+export default Tweet
